9dac990237fce1feb8612f46b2f749e5fcd8bc31
Stoia91 <stoia91@gmail.com>
2022-01-20 18:14:48 -0500

    Created User model

---
+++ src/main/java/com/revature/models/User.java
@@ -0,0 +1,103 @@
+package com.revature.models;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity //@Entity makes a Class a DB table
+@Table(name = "users") //@Table lets us change some table values
+public class User {
+	@Id //This will make the field a primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //this makes our PK serial
+	@Column(name = "user_id") //usually I'll just call a field what I want it to be called instead of doing this...
+	private int id;
+	
+	@Column(unique = true)
+	private String username;
+	
+	private String password;
+	
+	@Column(unique = true)
+	private String email;
+	
+	public User() {
+		super();
+	}
+
+	public User(String username, String password, String email) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
+
+	public User(int id, String username, String password, String email) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", email=" + email + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, id, password, username);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(email, other.email) && id == other.id && Objects.equals(password, other.password)
+				&& Objects.equals(username, other.username);
+	}
+	
+	
+}
