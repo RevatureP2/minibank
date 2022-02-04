2689022593b394dd8a5c8814771dd06d300953bd
jl1593 <junlinig@gmai.com>
2022-02-04 16:27:07 -0500

    reg page created

--- p2frontend/src/app/components/navbar/navbar.component.ts
+++ p2frontend/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
+import { NonNullAssert } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
 import { LoginpageComponent } from '../loginpage/loginpage.component';
 
 @Component({
@@ -7,10 +9,12 @@ import { LoginpageComponent } from '../loginpage/loginpage.component';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  user:any=null;
+  constructor(private ls:LoginService) { }
 
   ngOnInit(): void {
+    this.user=this.ls.user2;
+    console.log(this.user)
   }
 
 }
