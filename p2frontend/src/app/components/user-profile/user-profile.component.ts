1f3f5b5ea5fba483943cb8d6739c9d15f9d71d77
Stoia91 <stoia91@gmail.com>
2022-02-07 13:56:41 -0500

    added user info to user page

--- p2frontend/src/app/components/user-profile/user-profile.component.ts
+++ p2frontend/src/app/components/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
+import { LoginpageComponent } from '../loginpage/loginpage.component';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
-
-  constructor() { }
+user:any = null;
+  constructor(private ls:LoginService) { }
 
   ngOnInit(): void {
+    this.user=this.ls.user2;
+    console.log(this.user)
   }
 
 }
